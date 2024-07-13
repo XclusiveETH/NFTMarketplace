ebbd64e1250f2406463a8a58a596a8b7fd3585e1
kamalishere <kamaleinstein1@gmail.com>
2024-07-13 12:50:55 +0530

    fix build

--- const/contractAddresses.ts
+++ const/contractAddresses.ts
@@ -1,8 +1,12 @@
+import { DchainTestnet } from "@thirdweb-dev/chains";
+
 
 export const MARKETPLACE_ADDRESS = "0x8AC8bDF3982356bB24a3592CcB44ff494166172b";
 
 export const NFT_COLLECTION_ADDRESS = "0x18d6a7784E0c4Dc8f1098bf68B4eB7FaB6f2d5E7";
 
 export const RENTNFT_COLLECTION_ADDRESS = "";
+
+export const NETWORK=  DchainTestnet;
 
 export const EXPLORER_URL = "https://dchaintestnet-2713017997578000-1.testnet.sagaexplorer.io";
